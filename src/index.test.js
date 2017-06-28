019a814710a81ef518644ef11510efa28f938b29
Plazek <michael.plazek@hpe.com>
2017-06-28 17:34:31 -0600

    added additional tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
 import expect from 'expect';
 
+const MAX_DELAY = 10;
+
 describe('Our first suite', () => {
 
   it('test 1.1', (done) => {
@@ -18,22 +20,22 @@ describe('Our first suite', () => {
 describe('Our second suite', () => {
 
   it('test 2.1', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 
   it('test 2.2', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolTrue(done);}, num);
   });
 
   it('test 2.3', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 
   it('test 2.4', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 });
@@ -41,12 +43,12 @@ describe('Our second suite', () => {
 describe('Our third suite', () => {
 
   it('test 3.1', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 
   it('test 3.2', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 });
@@ -54,27 +56,27 @@ describe('Our third suite', () => {
 describe('Our fourth suite', () => {
 
   it('test 4.1', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolTrue(done);}, num);
   });
 
   it('test 4.2', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 
   it('test 4.3', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 
   it('test 4.4', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 
   it('test 4.5', (done) => {
-    let num = getRand(0, 2000);
+    let num = getRand(0, MAX_DELAY);
     setTimeout(function(){return getBoolFalse(done);}, num);
   });
 });
